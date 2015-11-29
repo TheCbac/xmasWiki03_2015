b1fb5619ceaaf69742cf4366422fffe3be76125e
Craig Cheney <ccheney@mit.edu>
2015-11-28 16:54:59 -0800

    Changed Email settings

--- server/collections.initialize.js
+++ server/collections.initialize.js
@@ -4,7 +4,7 @@ Meteor.startup(function() {
 	if (Meteor.users.find().count() === 0){
 		craigId = Accounts.createUser({
 			email:'ccheney@mit.edu',
-			password:'password',
+			password:"password",
 			profile: {
 				firstName: 'Craig',
 				lastName:'Cheney',
@@ -14,112 +14,70 @@ Meteor.startup(function() {
 			}
 		});
 
-		adrienneId = Accounts.createUser({
-			email:'adcheney88@gmail.com',
-			password:'password',
-			profile: {
-				firstName: 'Adrienne',
-				lastName:'Cheney',
-				groups:[cheneyClanId],
-				gender:"Female",
-			}
-		});
 
-		brianId = Accounts.createUser({
-			email:'briancheney2010@yahoo.com',
-			password:'password',
-			profile: {
-				firstName: 'Brian',
-				lastName:'Cheney',
-				groups:[cheneyClanId],
-				gender:"Male",
-			}
-		});
 
-		kathleenId = Accounts.createUser({
-			email:'kbuckcheney@comcast.net',
-			password:'password',
-			profile: {
-				firstName: 'Kathleen',
-				lastName:'Buck',
-				groups:[cheneyClanId],
-				gender:"Female",
-			}
-		});		
+		// adrienneId = Accounts.createUser({
+		// 	email:'adcheney88@gmail.com',
+		// 	profile: {
+		// 		firstName: 'Adrienne',
+		// 		lastName:'Cheney',
+		// 		groups:[cheneyClanId],
+		// 		gender:"Female",
+		// 	}
+		// });
+		// Accounts.sendEnrollmentEmail(adrienneId);
 
-		bruceId = Accounts.createUser({
-			email:'bwcheney@comcast.net',
-			password:'password',
-			profile: {
-				firstName: 'Bruce',
-				lastName:'Cheney',
-				groups:[cheneyClanId],
-				gender:"Male",
-			}
-		});
+		// brianId = Accounts.createUser({
+		// 	email:'briancheney2010@yahoo.com',
+		// 	profile: {
+		// 		firstName: 'Brian',
+		// 		lastName:'Cheney',
+		// 		groups:[cheneyClanId],
+		// 		gender:"Male",
+		// 	}
+		// });
+		// Accounts.sendEnrollmentEmail(brianId);
 
-		crisId = Accounts.createUser({
-			email:'cmc04747@pomona.edu',
-			password:'password',
-			profile: {
-				firstName: 'Cris',
-				lastName:'Cheney',
-				groups:[cheneyClanId],
-				gender:"Female",
-			}
-		});
+		// kathleenId = Accounts.createUser({
+		// 	email:'kbuckcheney@comcast.net',
+		// 	profile: {
+		// 		firstName: 'Kathleen',
+		// 		lastName:'Buck',
+		// 		groups:[cheneyClanId],
+		// 		gender:"Female",
+		// 	}
+		// });	
+		// Accounts.sendEnrollmentEmail(kathleenId);	
+
+		// bruceId = Accounts.createUser({
+		// 	email:'bwcheney@comcast.net',
+		// 	profile: {
+		// 		firstName: 'Bruce',
+		// 		lastName:'Cheney',
+		// 		groups:[cheneyClanId],
+		// 		gender:"Male",
+		// 	}
+		// });
+		// Accounts.sendEnrollmentEmail(bruceId);
+
+		// crisId = Accounts.createUser({
+		// 	email:'cmc04747@pomona.edu',
+		// 	profile: {
+		// 		firstName: 'Cris',
+		// 		lastName:'Cheney',
+		// 		groups:[cheneyClanId],
+		// 		gender:"Female",
+		// 	}
+		// });
+		// Accounts.sendEnrollmentEmail(crisId);
 	}
-
-	// var bowId,  kittyId, chocolateId, zoombaShoeId;
-	// if (items.find().count()===0){
-
-	// 	bowId = items.insert({
-	// 		owner: craigId,
-	// 		gifter: brianId,
-	// 		details: {
-	// 			name:"a cool bow",
-	// 			link: "www.google.com",
-	// 			cost: "$25.99"
-	// 		}
-	// 	});
-
-	// 	kittyId = items.insert({
-	// 		owner: craigId,
-	// 		gifter: adrienneId,
-	// 		details: {
-	// 			name: "a Big Kitty",
-	// 			link: "www.reuters.com",
-	// 			cost: "$7.99"
-	// 		}
-	// 	});
-
-	// 	chocolateId = items.insert({
-	// 		owner: brianId,
-	// 		gifter: adrienneId,
-	// 		details: {
-	// 			name: "Chocolate",
-	// 			link: "www.reuters.com",
-	// 			cost: "$7.99"
-	// 		}
-	// 	});
-
-	// 	zoombaShoeId = items.insert({
-	// 		owner: kathleenId,
-	// 		gifter: adrienneId,
-	// 		details: {
-	// 			name: "Zoomba Shoe",
-	// 			link: "www.jcpenny.com",
-	// 			cost: "$98"
-	// 		}
-	// 	});
-
-	// }
 
 		var cheneyClanId;
 	if (Groups.find().count()===0){
 		cheneyClanId = Groups.insert({
 			name:"CheneyClan",
-			members:[craigId, bruceId, brianId, adrienneId, kathleenId, crisId],
+			members:[craigId],
+			// members:[craigId, bruceId, brianId, adrienneId, kathleenId, crisId],
 			admins:[craigId]
 		});
 	}
--- server/registerNewUser.js
+++ server/registerNewUser.js
@@ -1,3 +1,13 @@
+Accounts.emailTemplates.siteName = "Xmas Wiki 2015";
+Accounts.emailTemplates.from = "Craig Cheney <ccheney@mit.edu>";
+Accounts.emailTemplates.enrollAccount.subject = function (user) {
+    return "Welcome to the Xmas Wiki";
+};
+
+Accounts.emailTemplates.enrollAccount.text = function (user, url) {
+   return "Please activate your Xmas Wiki account by simply click the link below:\n\n"+ url;
+};
+
 Meteor.methods({
 	createNewUser : function(firstIn, lastIn, emailIn, genderIn, clanIdIn){
 		newId = Accounts.createUser({
