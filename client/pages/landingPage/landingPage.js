e6ce0e2819fc8aa13c8e92d198c242a0735cf9d1
Craig Cheney <ccheney@mit.edu>
2015-12-02 10:55:12 -0500

    Added forgot password

--- client/pages/landingPage/landingPage.js
+++ client/pages/landingPage/landingPage.js
@@ -7,28 +7,59 @@ Template.landingPage.helpers({
 
 	errors: function(){
 		var errorId = Session.get("error");
-		return (errorId == "badPassword");
+		return errorId;
 	}
 });
 
 Template.landingPage.events({
 	'click #signInButton': function(event, template){
+		Session.set('error',"");
 		var userEmail = document.getElementById("email").value;
 		var userPassword = document.getElementById("password").value;
-		Meteor.loginWithPassword(userEmail, userPassword, passwordCallBack);
+		Meteor.loginWithPassword(userEmail, userPassword, loginCallBack);
 		event.preventDefault();
+	},
+
+	'click #forgotPasswordButton' : function(event, template){
+		event.preventDefault();
+		Session.set('error',"");
+		var userEmail = document.getElementById('email').value;
+		if(userEmail === ""){
+			Session.set("error", "Missing Email");
+		}
+		else{
+			Accounts.forgotPassword({email:userEmail}, forgotPasswordCallBack);	
+		}
+		
 	}
 });
 
-passwordCallBack = function(error,id){
+loginCallBack = function(error,id){
 	if (error){
-		if(error.error == 403){
-			Session.set("error","badPassword");
+		if(error.reason == "Incorrect password"){
+			Session.set("error","Incorrect password");
+		}
+		else{
+			Session.set('error', "An error occured");
 		}
 
 	}
 	
 	else{
 		Session.set("error", "");
+	}
+};
+
+forgotPasswordCallBack = function(error, id){
+	if(error){
+		if (error.reason =="User not found"){
+			Session.set("error", "Email not found");
+		}
+		else{
+			Session.set('error', "An error occured");
+		}
+	}
+	else{
+		Session.set("error", "An email has been sent");
 	}
 };
